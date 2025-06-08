6637f089c39de2f1dfb84c44878a5843cea7622e
a-murataly <130218411+a-murataly@users.noreply.github.com>
2025-06-08 18:23:22 +0000

    redesigned the architect to dynamic routing

---
+++ app/calculations.py
@@ -0,0 +1,10 @@
+def category_share(product_sales: float, category_sales: float) -> float:
+    return round(product_sales/category_sales, 3)
+
+category_share = {
+    "category_share": {
+        "func": category_share,
+        "args": ["product_sales", "category_sales"],
+        "description": "Calculates share of a product in category"
+    }
+}
--- app/main.py
+++ app/main.py
@@ -1,12 +1,6 @@
 from fastapi import FastAPI
-from app.schemas import CategoryShareInput
-app=FastAPI()
+from app.dynamic_router import router
 
-@app.get("/")
-def root():
-  return {"message": "Product Management Console is running!"}
+app = FastAPI()
 
-@app.post("/category_share")
-def calculate_share(data: CategoryShareInput):
-  share=data.product_sales_value_input/data.category_sales_value_input
-  return {"share": round(share,3)}
+app.include_router(router)
