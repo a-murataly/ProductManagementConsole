5e9bd42d867273c0a90685fbd6d8a8e9fdc694a2
a-murataly <130218411+a-murataly@users.noreply.github.com>
2025-06-09 19:10:05 +0000

    created scaling factors

--- app/routes/upload_csv.py
+++ app/routes/upload_csv.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, UploadFile, File
+from app.services.csv_handler import read_csv, decompose, calculate_scaling_factors
 
 router=APIRouter()
 @router.post("/upload_csv")
 async def upload_csv(file: UploadFile = File(...)):
-    contents = await file.read()
-    return {"filename": file.filename, "size_bytes": len(contents)}
+    df=await read_csv(file)
+    df,combined_cols=decompose(df)
+    scaling=calculate_scaling_factors(df,combined_cols)
+    return {"scaling factors:": scaling}
---
+++ app/services/csv_handler.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import io
+from fastapi import UploadFile
+
+async def read_csv(file: UploadFile):
+    contents=await file.read()
+    df=pd.read_csv(io.StringIO(contents.decode("utf-8")))
+    required_columns=["Date","City","Store","Category","Sub-category","Brand","Item","Sales qty","Sales value","Sales COGS"]
+    if not all(col in df.columns for col in required_columns):
+        missing = list(set(required_columns) - set(df.columns))
+        raise ValueError(f"Missing required columns: {', '.join(missing)}")
+    return {"rows": df.shape[0], "columns": df.shape[1]}
+
+def decompose(df):
+    levels = ["Date", "City", "Category", "Sub-category", "Brand", "Item"]
+    combined_cols = []
+
+    for i in range(1, len(levels) + 1):
+        col_name = "+".join(levels[:i])
+        df[col_name] = df[levels[:i]].astype(str).agg("|".join, axis=1)
+        combined_cols.append(col_name)
+
+    return df, combined_cols
+
+def calculate_scaling_factors(df, combined_cols):
+    scaling_factors=[]
+
+    for i in range(len(combined_cols)-1):
+        upper=combined_cols[i]
+        lower=combined_cols[i+1]
+        upper_count=df[upper].nunique()
+        lower_count=df[lower].nunique()
+        ratio=round(lower_count/upper_count,2) if upper_count else None
+
+        scaling_factors.append({
+            "from": upper,
+            "to":lower,
+            "unique_in_from": upper_count,
+            "unique_in_to": lower_count,
+            "scaling_factor": ratio
+        })
+    return scaling_factors
