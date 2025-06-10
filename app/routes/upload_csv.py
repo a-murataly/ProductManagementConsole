0d71ad38b210fd5260d56c23e6a6f3f835b2cc26
a-murataly <130218411+a-murataly@users.noreply.github.com>
2025-06-10 19:13:24 +0000

    loggin error in upload csv

--- app/routes/upload_csv.py
+++ app/routes/upload_csv.py
@@ -2,12 +2,18 @@ from fastapi import APIRouter, UploadFile, File
 from app.services.csv_handler import read_csv, decompose, calculate_scaling_factors
 
 router=APIRouter()
+
 @router.post("/upload_csv")
 async def upload_csv(file: UploadFile = File(...)):
     try:
-        df=await read_csv(file)
-        df,combined_cols=decompose(df)
-        scaling=calculate_scaling_factors(df,combined_cols)
-        return {"scaling factors:": scaling}
+        df = await read_csv(file)
+        df, combined_cols = decompose(df)
+        scaling = calculate_scaling_factors(df, combined_cols)
+
+        # временно логируем
+        print("✅ Scaling factors:", scaling)
+
+        return {"scaling_factors": scaling}
     except Exception as e:
+        print("❌ Ошибка:", str(e))
         return {"error": str(e)}
