d9a40a842990ebd27926401fd0e58ea851604349
a-murataly <130218411+a-murataly@users.noreply.github.com>
2025-06-10 18:54:05 +0000

    added html with jinja

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from app.routes import upload_csv
 
 app = FastAPI()
 
+# Подключаем маршруты
 app.include_router(upload_csv.router)
+
+# Указываем путь к шаблонам
+templates = Jinja2Templates(directory="app/templates")
+
+# Отображаем главную страницу
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
