055221b15821295e15fabb62ac07410447688230
a-murataly <130218411+a-murataly@users.noreply.github.com>
2025-06-12 17:44:56 +0000

    added baseline calculation

--- app/routes/upload_csv.py
+++ app/routes/upload_csv.py
@@ -7,13 +7,17 @@ router=APIRouter()
 async def upload_csv(file: UploadFile = File(...)):
     try:
         df = await read_csv(file)
+        df_baseline = df.copy()
+
         df, combined_cols = decompose(df)
-        scaling = calculate_scaling_factors(df, combined_cols)
+        df_baseline,_=decompose(df_baseline)
 
-        # временно логируем
-        print("✅ Scaling factors:", scaling)
-
-        return {"scaling_factors": scaling}
-    except Exception as e:
-        print("❌ Ошибка:", str(e))
+        filtered = calculate_scaling_factors(df, combined_cols)
+        baseline = calculate_scaling_factors(df_baseline, combined_cols)
+        
+        return {
+            "filtered_factors": filtered,
+            "baseline_factors": baseline
+            }
+    except Exception as e:        
         return {"error": str(e)}
